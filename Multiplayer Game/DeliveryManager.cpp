5cd6583624af6be94b95a89ba9df0ff558758f4e
Lorién <lorienportella@gmail.com>
2019-11-14 19:06:57 +0100

    delivery manager wip

--- Multiplayer Game/DeliveryManager.cpp
+++ Multiplayer Game/DeliveryManager.cpp
@@ -1,2 +1,21 @@
 #include "Networks.h"
+#include "DeliveryManager.h"
 
+
+
+DeliveryManager::DeliveryManager()
+{
+}
+
+
+DeliveryManager::~DeliveryManager()
+{
+}
+
+Delivery * DeliveryManager::writeSequenceNumber(OutputMemoryStream & packet)
+{
+	packet << seq_number;
+	seq_number++;
+
+	return nullptr;
+}
--- Multiplayer Game/DeliveryManager.h
+++ Multiplayer Game/DeliveryManager.h
@@ -1,2 +1,42 @@
 #pragma once
+class DeliveryManager;
+
+class DeliveryDelegate
+{
+public:
+	virtual void onDeliverySuccess(DeliveryManager* deliveryManager) = 0;
+	virtual void onDeliveryFailure(DeliveryManager* deliveryManager) = 0;
+};
+
+struct Delivery
+{
+	uint32 sequenceNumber = 0;
+	double dispatchTime = 0.0;
+	DeliveryDelegate* delegate = nullptr;
+};
+class DeliveryManager
+{
+public:
+	DeliveryManager();
+	~DeliveryManager();
+
+	Delivery* writeSequenceNumber(OutputMemoryStream& packet);
+
+	bool processSequenceNumber(const InputMemoryStream& packet);
+
+	bool hasSequenceNumbersPendingAck()const;
+	void writeSequenceNumbersPendingAck(OutputMemoryStream& packet);
+
+	void processAckdSequenceNumbers(const InputMemoryStream& packet);
+	void processTimedOutPackets();
+
+	void clear();
+
+private:
+
+	uint32 seq_number = 0;
+	std::vector<Delivery*> pending_deliveries;
+
+};
+
 
